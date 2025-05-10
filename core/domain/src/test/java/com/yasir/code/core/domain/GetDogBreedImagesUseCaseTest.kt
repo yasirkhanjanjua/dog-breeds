5d3f5f468716d6ba389a9c2a8308ed88f09d18f5
Yasir Khan <yasirkhanjanjua@gmail.com>
2025-05-10 21:48:20 +0100

    GetDogBreedImagesUseCase tests

--- core/domain/src/test/java/com/yasir/code/core/domain/GetDogBreedImagesUseCaseTest.kt
+++ core/domain/src/test/java/com/yasir/code/core/domain/GetDogBreedImagesUseCaseTest.kt
@@ -7,19 +7,15 @@ import com.yasir.code.core.domain.model.Result
 import com.yasir.code.core.domain.repository.DogBreedImagesRepository
 import io.mockk.every
 import io.mockk.mockk
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.instanceOf
 import org.hamcrest.Matchers.`is`
-
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 
-@OptIn(ExperimentalCoroutinesApi::class)
 class GetDogBreedImagesUseCaseTest {
     private val mockDogBreedImagesRepository: DogBreedImagesRepository = mockk()
     private lateinit var sut: GetDogBreedImagesUseCase
